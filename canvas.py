961a9953b53eb1339a5483bd703d5cd83a68d5ee
= <sultaninstem004@gmail.com>
2025-01-15 22:17:36 -0800

    setup

--- canvas.py
+++ canvas.py
@@ -7,4 +7,18 @@ class Canvas:
         pygame.display.set_caption("Fourier Series")
         self.running = True
         self.clock = pygame.time.Clock()
-        
+        self.fps = 60 
+
+    def handle_events(self): 
+        for event in pygame.event.get(): 
+            if event.type == pygame.QUIT: 
+                self.running = False 
+
+    def update(self): 
+        pass 
+
+    def render(self): 
+        self.screen.fill((0,0,0))
+
+        pygame.display.flip()
+        self.clock.tick(self.fps)
