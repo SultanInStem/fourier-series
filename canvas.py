1c69b9f42f39c5cd386c1b44ed0dc3c6a437102e
= <sultaninstem004@gmail.com>
2025-01-15 22:15:06 -0800

    minor

--- canvas.py
+++ canvas.py
@@ -1,1 +1,10 @@
 import pygame
+from config import SCREEN_SIZE
+class Canvas: 
+    def __init__(self): 
+        pygame.init()
+        self.screen = pygame.display.set_mode(SCREEN_SIZE)
+        pygame.display.set_caption("Fourier Series")
+        self.running = True
+        self.clock = pygame.time.Clock()
+        
